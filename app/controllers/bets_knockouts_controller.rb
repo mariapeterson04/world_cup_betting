de43167e72438f9f3f26e60b552b6b0ee6db157a
Raghu Betina <raghu@firstdraft.com>
2018-06-02 04:22:24 +0000

    Only bets_knockout user can modify bets_knockout

--- app/controllers/bets_knockouts_controller.rb
+++ app/controllers/bets_knockouts_controller.rb
@@ -1,4 +1,14 @@
 class BetsKnockoutsController < ApplicationController
+  before_action :current_user_must_be_bets_knockout_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_bets_knockout_user
+    bets_knockout = BetsKnockout.find(params[:id])
+
+    unless current_user == bets_knockout.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @bets_knockouts = BetsKnockout.all
 
