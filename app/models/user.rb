bb43de99399e6dacbc82922697695bbcb883b590
Raghu Betina <raghu@firstdraft.com>
2018-06-02 04:21:55 +0000

    Direct association between users and bets_knockouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :bets_knockouts,
+             :dependent => :destroy
+
   has_many   :bets_groups,
              :dependent => :destroy
 
