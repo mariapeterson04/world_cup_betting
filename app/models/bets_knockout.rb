b6530b9c3bca19c5cf3295ee04f1dd8b3fa437ee
Raghu Betina <raghu@firstdraft.com>
2018-06-02 04:21:55 +0000

    Direct association between matches_knockouts and bets_knockouts

--- app/models/bets_knockout.rb
+++ app/models/bets_knockout.rb
@@ -1,6 +1,10 @@
 class BetsKnockout < ApplicationRecord
   # Direct associations
 
+  belongs_to :user_signed,
+             :class_name => "MatchesKnockout",
+             :foreign_key => "match_id"
+
   belongs_to :user
 
   # Indirect associations
--- app/models/matches_knockout.rb
+++ app/models/matches_knockout.rb
@@ -1,6 +1,10 @@
 class MatchesKnockout < ApplicationRecord
   # Direct associations
 
+  has_many   :bets_knockouts,
+             :foreign_key => "match_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
