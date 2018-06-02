c1a62789bfacfda350076cdfe3cbf03ad749d026
Raghu Betina <raghu@firstdraft.com>
2018-06-02 04:22:05 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "bets_groups#index"
   # Routes for the Bets_knockout resource:
   # CREATE
