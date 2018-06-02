252148e819b43f072e1d3889b4045397ab7fddc1
Raghu Betina <raghu@firstdraft.com>
2018-06-02 04:21:39 +0000

    Generate bets_groups resource with starter:resource

---
+++ app/controllers/bets_groups_controller.rb
@@ -0,0 +1,93 @@
+class BetsGroupsController < ApplicationController
+  def index
+    @bets_groups = BetsGroup.all
+
+    render("bets_groups/index.html.erb")
+  end
+
+  def show
+    @bets_group = BetsGroup.find(params[:id])
+
+    render("bets_groups/show.html.erb")
+  end
+
+  def new
+    @bets_group = BetsGroup.new
+
+    render("bets_groups/new.html.erb")
+  end
+
+  def create
+    @bets_group = BetsGroup.new
+
+    @bets_group.user_id = params[:user_id]
+    @bets_group.match_id = params[:match_id]
+    @bets_group.bet_team_one = params[:bet_team_one]
+    @bets_group.bet_team_two = params[:bet_team_two]
+    @bets_group.team_one = params[:team_one]
+    @bets_group.team_two = params[:team_two]
+    @bets_group.date = params[:date]
+    @bets_group.winner = params[:winner]
+
+    save_status = @bets_group.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/bets_groups/new", "/create_bets_group"
+        redirect_to("/bets_groups")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Bets group created successfully.")
+      end
+    else
+      render("bets_groups/new.html.erb")
+    end
+  end
+
+  def edit
+    @bets_group = BetsGroup.find(params[:id])
+
+    render("bets_groups/edit.html.erb")
+  end
+
+  def update
+    @bets_group = BetsGroup.find(params[:id])
+
+    @bets_group.user_id = params[:user_id]
+    @bets_group.match_id = params[:match_id]
+    @bets_group.bet_team_one = params[:bet_team_one]
+    @bets_group.bet_team_two = params[:bet_team_two]
+    @bets_group.team_one = params[:team_one]
+    @bets_group.team_two = params[:team_two]
+    @bets_group.date = params[:date]
+    @bets_group.winner = params[:winner]
+
+    save_status = @bets_group.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/bets_groups/#{@bets_group.id}/edit", "/update_bets_group"
+        redirect_to("/bets_groups/#{@bets_group.id}", :notice => "Bets group updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Bets group updated successfully.")
+      end
+    else
+      render("bets_groups/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @bets_group = BetsGroup.find(params[:id])
+
+    @bets_group.destroy
+
+    if URI(request.referer).path == "/bets_groups/#{@bets_group.id}"
+      redirect_to("/", :notice => "Bets group deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Bets group deleted.")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Bets_group resource:
+  # CREATE
+  get "/bets_groups/new", :controller => "bets_groups", :action => "new"
+  post "/create_bets_group", :controller => "bets_groups", :action => "create"
+
+  # READ
+  get "/bets_groups", :controller => "bets_groups", :action => "index"
+  get "/bets_groups/:id", :controller => "bets_groups", :action => "show"
+
+  # UPDATE
+  get "/bets_groups/:id/edit", :controller => "bets_groups", :action => "edit"
+  post "/update_bets_group/:id", :controller => "bets_groups", :action => "update"
+
+  # DELETE
+  get "/delete_bets_group/:id", :controller => "bets_groups", :action => "destroy"
+  #------------------------------
+
   devise_for :users
   # Routes for the User resource:
   # READ
